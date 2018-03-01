a7e083dcc22e5d123115acd12551a454cefd351a
drsaagrawal <drsaagrawal@gmail.com>
2018-03-01 10:44:08 +0530

    Update model.py

--- model.py
+++ model.py
@@ -105,11 +105,12 @@ from keras.layers import Flatten,Dense,Lambda,Cropping2D,Dropout
 from keras.layers.convolutional import Convolution2D
 
 model = Sequential()
-model.add(Lambda(lambda x : x / 255.0-0.5,input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Cropping2D(cropping=((70,25),(0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x : x / 255.0-0.5))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Dropout(0.95))
 model.add(Convolution2D(68,3,3,activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Flatten())
